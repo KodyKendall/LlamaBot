cc2dd6041962d09a66eb4cf436088e80020d7da1
KodyKendall <kodyckendall@gmail.com>
2025-10-15 07:04:09 -0600

    Progress on frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderWidth: {
+        '3': '3px',
+      },
+    },
+  },
+  plugins: [],
+}
